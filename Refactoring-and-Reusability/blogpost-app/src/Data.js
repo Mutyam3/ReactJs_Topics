d80db12197eaee7ddbdf33677cc56e585a8caa5d
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-09-01 18:17:43 +0530

    blogspost commit

---
+++ Refactoring-and-Reusability/blogpost-app/src/Data.js
@@ -0,0 +1,30 @@
+const postDetails = [
+                      {
+                          title : "Night cafe",
+                          content : `NightCafe is one of the most popular AI art generators
+                                     on the market. The tool is said to have more algorithms and 
+                                     art-generation features than other AI art generators`,
+                          likes: 1542,
+                          dislikes : 120,
+                          image : "./../images/NightCafe.jpg"
+
+                      },
+                      {
+                         title : "A Cozy Kitchen",
+                         content : `It’s just a cooking blog about a girl and her dog.
+                                    The site’s design is sleek, cute, and each blog post retains a recipe recounted by the author’s experiences.`,
+                         likes : 1450,
+                         dislikes : 250,
+                         image : "./../images/Kitchen blog.jpg"
+                      },
+                      {
+                          title  : "Young House Love",
+                          content : `The first post at Young House Love offers a quick glimpse into what’s to come from bloggers John and Sherry.
+                                     It’s quick and to the point, and acts more of a preview than a full introduction to the couple and their blog.`,
+                          likes : 7502,
+                          dislikes : 410,
+                          image : "./../images/House.jpg"
+                      }
+                    ]
+
+export default postDetails
