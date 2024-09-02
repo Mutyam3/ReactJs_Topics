9406a19cb9bfcef88a7711c60c45859352525470
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-09-02 11:48:08 +0530

    select players commit

---
+++ Refactoring-and-Reusability/select-players/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Player from './Player';
+
+function App() {
+  return (
+    <div className="App">
+           <h1>Select Players For Today Match</h1>
+           <Player/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Refactoring-and-Reusability/select-players/src/Data.js
@@ -0,0 +1,9 @@
+
+const playersDetails = [   
+                           "Pat Cummins", "Abhishek Sharma","Travis Head", "Nitish Kumar Reddy",
+                           "Heinrich Klaasen", "Aiden Markram", "Bhuvenshwar Kumar", "Shahbaz Ahmed",
+                           "Fazalhaq Farooqi", "Mayank Agarwal", "Glenn Philips", "Washington Sundar",
+                           "Abdul Samad", "Rahul Tripathi", "Umran Malik","T.Natarajan", "Jaydev Unadkat"
+                       ]
+
+export default playersDetails
