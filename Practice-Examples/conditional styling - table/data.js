e8ceaa22764600e53ffc837081b95b5eebc1e8e9
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-09-15 21:42:02 +0530

    conditional styling-table

---
+++ Practice-Examples/conditional styling - table/data.js
@@ -0,0 +1,30 @@
+ const userDetails = [
+    {
+         firstName : 'Mutyam',
+         lastName : 'Bhargav',
+         age : 25,
+         gender : 'Male',
+         course : 'Java'
+    },
+    {
+        firstName : 'Ravali',
+        lastName : 'Reddy',
+        age : 45,
+        gender : 'Female',
+        course : 'Python'
+    },
+    {
+        firstName : 'Sanju',
+        lastName : 'sahu',
+        age : 38,
+        gender : 'Male',
+        course : 'Mern'
+    },
+    {
+        firstName : 'priya',
+        lastName : 'mani',
+        age : 23,
+        gender : 'FeMale',
+        course : 'AI'
+
+    }]
