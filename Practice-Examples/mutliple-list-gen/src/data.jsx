0d0a93fd21220d18f871ca47e0be6d60dfea914e
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-09-15 18:22:30 +0530

    list-gen commit

---
+++ Practice-Examples/mutliple-list-gen/src/data.jsx
@@ -0,0 +1,43 @@
+
+export const listDetails = [
+                            {
+                               title :'Group1',
+                               items : ['India', 'Srilanka','SouthAfrica','Bangladesh']
+                            },
+                            {
+                                title : 'Group2',
+                                items : ['Russia', 'Ukrain', 'Itali','Mayanmar']
+                            },
+                            {
+                                title : 'Group3',
+                                items : ['NewZealand','WestIndies', 'England','Scotland']
+                            },
+                            {
+                                title : 'Group4',
+                                items : ['Australia', 'Canada','Portugal','Chile']
+                            },
+                            {
+                                title : 'Group5',
+                                items : ['Canada', 'USA', 'China', 'Russia']
+                            }, 
+                            {
+                                title : 'Group6',
+                                items : ['Canada', 'USA', 'China', 'Russia']
+                            },
+                            {
+                                title : 'Group7',
+                                items : ['Canada', 'USA', 'China', 'Russia']
+                            },
+                            {
+                                title : 'Group8',
+                                items : ['India', 'Srilanka','SouthAfrica','Bangladesh']
+                            },
+                            {
+                                title : 'Group9',
+                                items : ['Canada', 'USA', 'China', 'Russia']
+                            },
+                            {
+                                title : 'Group10',
+                                items :  ['Canada', 'USA', 'China', 'Russia']
+                            }
+                           ]
