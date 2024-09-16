170fb0603230a577a0c605d4b1ade95ae2809b0e
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-09-16 10:12:35 +0530

    class based

---
+++ Practice-Examples/conditional styling-class based/data.js
@@ -0,0 +1,39 @@
+ const userDetails = [
+    {
+         firstName : 'Mutyam',
+         lastName : 'Bhargav',
+         gender : 'Male',
+         course : 'Java'
+    },
+    {
+        firstName : 'Maha',
+        lastName : 'Raja',
+        gender : 'Male',
+        course : 'Mean'
+     },
+    {
+        firstName : 'Ravali',
+        lastName : 'Reddy',
+        gender : 'Female',
+        course : 'Python'
+    },
+    {
+        firstName : 'Sanju',
+        lastName : 'sahu',
+        gender : 'Male',
+        course : 'Mern'
+    },
+    {
+        firstName : 'priya',
+        lastName : 'mani',
+        gender : 'Female',
+        course : 'AI'
+
+    },
+    {
+        firstName : 'Maha',
+        lastName : 'Raja',
+        gender : 'Male',
+        course : 'Machine Learning'
+     }
+    ]
