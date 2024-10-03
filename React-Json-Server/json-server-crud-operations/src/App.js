c60cf16a272209c9272e026fa466c5341e59ea90
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-10-03 14:20:45 +0530

    json-server-basics-crud

---
+++ React-Json-Server/json-server-crud-operations/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Books from './Components/BooksCompo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mutyam JSON-SERVER</h1>
+      <Books></Books>
+    </div>
+  );
+}
+
+export default App;
