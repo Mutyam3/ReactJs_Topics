d31a43a2e961cc479eb122fffe06175637ba5e68
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-11-10 11:57:07 +0530

    match commit

--- Redux-Toolkit/mutyam-cricscore-app/src/features/Body/Matches/CreateMatch.jsx
+++ Redux-Toolkit/mutyam-cricscore-app/src/features/Body/Matches/CreateMatch.jsx
@@ -5,18 +5,17 @@ import { Outlet, Link } from "react-router-dom";
 function CreateMatch()
 {
     return(
-        <>
-{/* 
-             <section className="box marginMania d-flex justify-content-evenly  border border-light  rounded">
-                       <Link to='' className="navbartext"><li className="m-2 p-2" type='none'>ADD PLAYER</li></Link>
-                       <Link to='' className="navbartext"><li className="m-2 p-2" type='none'>ADD TEAM</li></Link>
-                       <Link to='' className="navbartext"><li className="m-2 p-2" type='none'>ADD MATCH</li></Link>
+        <section>
 
+               <div className="m-2 p-2">
+                    
+               </div>
 
-             </section> */}
+               <div className="m-2 p-2">
 
-                      <Outlet/>
-        </>
+               </div>
+                      
+        </section>
     )
 }
 
